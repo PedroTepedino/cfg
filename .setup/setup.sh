142e40254c14960d81efd516d6b53eb3ed08b7db
tepedino <pedrotepedinossauro@gmail.com>
2022-09-28 20:41:11 -0300

    update setup Get sh files from github if not present in the system

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -4,7 +4,27 @@ sudo true
 
 cd ~
 
-source ./paru.sh
-source ./packages.sh
-source ./shell.sh
-source ./dotfiles.sh
+if [ -d "$HOME/.setup/" ]; then
+
+  setup_folder="$HOME/.setup"
+
+  paru="$setup_folder/paru.sh"
+  packages="$setup_folder/packages.sh"
+  shell="$setup_folder/shell.sh"
+  dotfiles="$setup_folder/dotfiles.sh"
+
+else
+
+  url="curl -L https://raw.githubusercontent.com/PedroTepedino/cfg/master/.setup"
+
+  paru=$("$url/setup.sh")
+  packages=$("$url/packages.sh")
+  shell=$("$url/shell.sh")
+  dotfiles=$("$url/dotfiles.sh")
+
+fi
+
+source $paru
+source $packages
+source $shell
+source $dotfiles
