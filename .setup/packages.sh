ba0f7b698b5a39fe51cfb26dc7f0ba8bc4c38354
tepedino <pedrotepedinossauro@gmail.com>
2022-09-28 21:00:15 -0300

    Compile setup scripts into one

--- .setup/packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source ./funcs.sh
-
-packages="git neovim alacritty rate-mirrors xorg xorg-xinit picom firefox awesome glow xf86-video-fbdev"
-
-echo "Installing packages"
-
-instal $packages
--- .setup/setup.sh
+++ .setup/setup.sh
@@ -1,30 +1,83 @@
 #!/bin/sh
 
-sudo true
+# funcs
+instal () {
+  if pacman -Qs paru > /dev/null; then
+    echo "paru NOT found"
+    echo "using pacman instead"
 
+    sudo pacman -Syu --needed --noconfirm $1
+  else
+    paru -Syu --needed --noconfirm $1
+  fi
+}
+
+
+#setup 
+sudo true
 cd ~
 
-if [ -d "$HOME/.setup/" ]; then
 
-  setup_folder="$HOME/.setup"
+#paru
+if pacman -Qs paru > /dev/null; then
+  echo "paru Already installed"
+else 
+  echo "Installing PARU"
+  sudo pacman -S --needed base-devel  
+  
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  
+  makepkg -si
+  cd ..
 
-  paru="$setup_folder/paru.sh"
-  packages="$setup_folder/packages.sh"
-  shell="$setup_folder/shell.sh"
-  dotfiles="$setup_folder/dotfiles.sh"
-
-else
-
-  url="curl -L https://raw.githubusercontent.com/PedroTepedino/cfg/master/.setup"
-
-  paru=$("$url/setup.sh")
-  packages=$("$url/packages.sh")
-  shell=$("$url/shell.sh")
-  dotfiles=$("$url/dotfiles.sh")
-
+  echo "Paru Installed"
+  echo "Removing Repo"
+  rm -rf paru
 fi
 
-source $paru
-source $packages
-source $shell
-source $dotfiles
+
+#packages
+packages="git neovim alacritty rate-mirrors xorg xorg-xinit picom firefox awesome glow xf86-video-fbdev"
+echo "Installing packages"
+instal $packages
+
+
+#shell
+packages="zsh ttf-meslo-nerd-font-powerlevel10k"
+install $packages
+
+if [ -d $HOME/.oh-my-zsh/ ]; then
+  echo "ohmyzsh already installed"
+else
+  echo "installing ohmyzsh"
+  sh -c "$(curl -fsoh https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+  echo "powerlevel10k already cloned"
+else
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+
+#dotfiles
+git clone --bare https://github.com/PedroTepedino/.dotfiles.git $HOME/.dotfiles
+
+function config {
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .config-backup
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+else
+  echo "Backing up pre-existing dot files.";
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv  {} .config-backup/{}
+fi;
+
+config checkout
+confi config status.showUntrackedFiles no
+
