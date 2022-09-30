c7d8687bdb4fb297ae1336c9b2c0feef9e2a31a7
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2022-09-30 15:40:40 -0300

    remove force checkout from dotfiles

--- .setup/cfg.sh
+++ .setup/cfg.sh
@@ -6,15 +6,15 @@ function config {
 }
 
 mkdir -p .config-backup
-config checkout -s
+config checkout
 
 if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  config checkout -f 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv  {} .config-backup/{}
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv  {} .config-backup/{}
 fi;
 
-config checkout -f
+config checkout
 config config status.showUntrackedFiles no
 
